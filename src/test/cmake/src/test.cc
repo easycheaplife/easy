0a686f73fde78985d5f6608ca0b11e10feba0be0
root <root@localhost.localdomain>
2014-08-29 22:14:52 +0800

    add use static lib example for cmake

---
+++ src/test/cmake/src/test.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"myadd.h"
+#include"mysub.h"
+
+int main()
+{
+	int a = 3;
+	int b = 4;
+	printf("%d+%d=%d %d-%d=%d\n", a, b, myadd(a, b), a, b, mysub(a, b));
+	return 0;
+}
