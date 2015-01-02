388b40466d0ae387f199841f17c911d44235f081
lee <lee@localhost.localdomain>
2015-01-02 09:16:26 +0800

    del mongo-c-driver

--- dep/mongo-c-driver/build_x86.bat
+++
@@ -1,8 +0,0 @@
-  cd src\libbson
-  cmake -DCMAKE_INSTALL_PREFIX=C:\usr -G "Visual Studio 11" .
-  msbuild.exe ALL_BUILD.vcxproj
-  msbuild.exe INSTALL.vcxproj
-  cd ..\..
-  cmake -DCMAKE_INSTALL_PREFIX=C:\usr -DBSON_ROOT_DIR=C:\usr -G "Visual Studio 11" .
-  msbuild.exe ALL_BUILD.vcxproj
-  msbuild.exe INSTALL.vcxproj
